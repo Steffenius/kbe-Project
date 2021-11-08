817e5d22b20fd0f912511790f912dc5315991a03
Steffenius <steffen_droessler@web.de>
2021-11-08 15:52:00 +0100

    basics in https://spring.io/guides/tutorials/rest/ *Evolving REST APIs -> changes due to split name in firstname and lastname

---
+++ src/main/java/payroll/LoadDatabase.java
@@ -0,0 +1,29 @@
+package payroll;
+
+/**
+ * @author Hitzek, Steffen; Matr.Nr 566507
+ * @author Schöbel, Susann; Matr.Nr 571657
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+        // am Error like "Could not autowire. No beans of 'EmployeeRepository' type found." can be ignored
+
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
+            log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
+            log.info("Preloading " + repository.save(new Employee("Gandalf", "Graurock", "wizard")));
+        };
+    }
+}
