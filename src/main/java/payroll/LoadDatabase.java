b10dbe173356d0a76339707d87881a7bd47d3854
Steffenius <steffen_droessler@web.de>
2021-11-09 11:30:13 +0100

    basics in https://spring.io/guides/tutorials/rest/ *Building links into your REST API

--- src/main/java/payroll/LoadDatabase.java
+++ src/main/java/payroll/LoadDatabase.java
@@ -16,14 +16,28 @@ class LoadDatabase {
 
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
+
     @Bean
-    CommandLineRunner initDatabase(EmployeeRepository repository) {
-        // am Error like "Could not autowire. No beans of 'EmployeeRepository' type found." can be ignored
+    CommandLineRunner initDatabase(@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") EmployeeRepository employeeRepository,
+                                   @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection") OrderRepository orderRepository) {
+        // an Error like "Could not autowire. No beans of 'ClassRepository' type found." can be ignored,
+        // because it exists in ClassRepository-Interface.
+        // see @ EmployeeRepository (interface) or/and  OrderRepository (interface)
 
         return args -> {
-            log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
-            log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
-            log.info("Preloading " + repository.save(new Employee("Gandalf", "Graurock", "wizard")));
+            log.info("Preloading " + employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar")));
+            log.info("Preloading " + employeeRepository.save(new Employee("Frodo", "Baggins", "thief")));
+            log.info("Preloading " + employeeRepository.save(new Employee("Gandalf", "Graurock", "wizard")));
+
+            //employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+
+            orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+            orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+            orderRepository.findAll().forEach(order -> {
+                log.info("Preloaded " + order);
+            });
         };
-    }
+    };
 }
