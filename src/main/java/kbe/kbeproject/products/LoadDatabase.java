c6bcb7aae84cb3a3ef0a830711af2cf1f5711a24
Steffenius <steffen_droessler@web.de>
2021-11-18 19:00:50 +0100

    removed a self-instructed Product in database init

--- src/main/java/kbe/kbeproject/products/LoadDatabase.java
+++ src/main/java/kbe/kbeproject/products/LoadDatabase.java
@@ -32,18 +32,7 @@ class LoadDatabase {
         // because it exists in ClassRepository-Interface.
         // see @ ProductRepository (interface)
         return args -> {
-            log.info("Preloading " + productRepository.save(
-                    new Product("phone",
-                                1500,
-                                "blau",
-                                6.0,
-                                "A15",
-                                "150GB",
-                                3,
-                                "Berlin",
-                                "Tolles Phone",
-                                "krasse Cam",
-                                2020)));
+
             // puts every Object from productList in Database
             productList.forEach(product -> log.info("Preloading " + productRepository.save(product)));
             productRepository.findAll().forEach(product -> log.info("Preloaded " + product));
